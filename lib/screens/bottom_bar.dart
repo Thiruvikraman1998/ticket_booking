9a43808b708d0dda5cdc68301cb80b883266c9a4
Thiruvikraman1998 <development.thiru@gmail.com>
2023-03-07 15:49:08 +0530

    Added navbar

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 
 class BottomBar extends StatefulWidget {
   const BottomBar({Key? key}) : super(key: key);
@@ -8,8 +9,58 @@ class BottomBar extends StatefulWidget {
 }
 
 class _BottomBarState extends State<BottomBar> {
+  int _selectIndex = 0;
+  static final List<Widget> _widgetsList = <Widget>[
+    const Text("Home"),
+    const Text("Search"),
+    const Text("Tickets"),
+    const Text("Profile"),
+  ];
+
+  void _onTapItem(int index) {
+    setState(() {
+      _selectIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Ticket App"),
+        ),
+        body: Center(
+          child: _widgetsList[_selectIndex],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: _onTapItem,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          selectedItemColor: Colors.blueGrey,
+          unselectedItemColor: Colors.red,
+          elevation: 10,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(FluentIcons.home_12_regular),
+                activeIcon: Icon(FluentIcons.home_12_filled),
+                label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                activeIcon: Icon(Icons.search),
+                label: "Search"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.airplane_ticket_outlined),
+                activeIcon: Icon(Icons.airplane_ticket),
+                label: "Ticket"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_2_outlined),
+                activeIcon: Icon(Icons.person_2),
+                label: "Profile"),
+          ],
+        ),
+      ),
+    );
   }
 }
